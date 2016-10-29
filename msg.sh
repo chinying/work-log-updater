7ed13ef620e5807546b9212e11e9fea63c833efe
Seah Chin Ying <scy.6char@gmail.com>
2016-10-29 22:10:39 +0800

    changed git log output

--- msg.sh
+++ msg.sh
@@ -1,4 +1,4 @@
 REPO_PATH="" # fill in with your repo path
 CUR_DIR=$(pwd)
 DATE=`date +%Y-%m-%d`
-cd $REPO_PATH && git log --since="3 days ago" --oneline > $CUR_DIR/logs/date-$DATE-out.txt
+cd $REPO_PATH && git log --since="1 day ago" --pretty=format:'%h %ad %s (%an)' --date=short > $CUR_DIR/logs/date-$DATE-out.txt
